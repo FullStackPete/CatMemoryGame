0a2e36787acecd26c2cc463198585b91c46d9fbd
Piotr Śliwa <psliwa.b@gmail.com>
2023-12-27 01:14:11 +0100

    Added lose screen animation

---
+++ src/components/Lose.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+function Lose({setLose, lose}) {
+    const [slide,setSlide] = useState(false);
+
+    useEffect(()=>{
+        setSlide(true);
+        setTimeout(()=>{
+            setSlide(false)
+        },1500)
+        
+    },[lose])
+  return (
+    <>
+      <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur">
+        <div
+          className={`absolute text-6xl w-max text-center ${slide ? "show" : "hide"}
+             top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2`}
+        >
+          <p className="pacifico">Try again!</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Lose;
